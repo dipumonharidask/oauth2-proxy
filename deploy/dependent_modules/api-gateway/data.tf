73a4850b12f389b68249c99006571c1639e9ec3e
aswin-in-philips <aswin.s@philips.com>
2022-05-14 12:32:42 +0530

    removing Prometheus

--- deploy/dependent_modules/api-gateway/data.tf
+++ deploy/dependent_modules/api-gateway/data.tf
@@ -22,8 +22,3 @@ data "cloudfoundry_service" "data_redis_service" {
 data "cloudfoundry_service" "vault" {
   name = "hsdp-vault"
 }
-
-data "cloudfoundry_app" "Prometheus" {
-  name_or_id = "3ad08628-ae45-40b5-8ae7-f039b5f82b8f"
-  space      = var.cf_space
-}
